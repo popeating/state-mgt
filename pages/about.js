3db86d7f3c575fdc3631d946ff8452a19d26c41f
popeating <pop@popeating.it>
2022-04-10 18:45:43 +0200

    Inital Release

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import useCart from '../store/store';
+import Header from '../components/Header';
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <div className="container mx-auto pt-4">ABOUT PAGE</div>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,62 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import useCart from '../store/store';
+import Header from '../components/Header';
+import { useEffect, useState } from 'react';
+import { products } from '../lib/products';
+import { Image } from 'next/image';
 
 export default function Home() {
+  const total = useCart((state) => state.total);
+  const addTocart = useCart((state) => state.addTocart);
+  const updatecart = useCart((state) => state.updatecart);
+  const mycart = useCart((state) => state.cartContent);
+  const addProduct = (params) => {
+    const product = mycart.findIndex((item) => item.id === params.id);
+    if (product !== -1) {
+      mycart[product].quantity++;
+      updatecart({ params, mycart });
+    } else {
+      addTocart(params);
+    }
+  };
+  const [mytotal, setTotal] = useState();
+  useEffect(() => {
+    setTotal(total);
+  }, [total]);
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <>
+      <Header />
+      <div className="container mx-auto pt-4">
+        <div className="pb-4">PRODUCTS</div>
+        <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          {products.map((product) => (
+            <a
+              key={product.id}
+              href="#"
+              onClick={() =>
+                addProduct({
+                  id: product.id,
+                  name: product.name,
+                  price: product.price,
+                  quantity: 1,
+                })
+              }
+              className="group"
+            >
+              <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+                <img
+                  src={product.imageSrc}
+                  alt={product.imageAlt}
+                  className="w-full h-full object-center object-cover group-hover:opacity-75"
+                />
+              </div>
+              <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
+              <p className="mt-1 text-lg font-medium text-gray-900">
+                ${product.price}
+              </p>
+            </a>
+          ))}
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+      </div>
+    </>
+  );
 }
